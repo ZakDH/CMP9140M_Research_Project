987ef4e99fd9115f2b2285db189dc147f4d6e3e9
ZakDH <zhargreaves1@gmail.com>
2023-07-23 22:48:07 +0100

    subnets

--- main.tf
+++ main.tf
@@ -12,7 +12,43 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example"
-  location = "West Europe"
+resource "azurerm_resource_group" "project-rg" {
+  name     = "project-resources"
+  location = "UK South"
+  tags = {
+    environment = "dev"
+  }
+}
+
+
+resource "azurerm_virtual_network" "project-vn" {
+  name                = "project-network"
+  location            = azurerm_resource_group.project-rg.location
+  resource_group_name = azurerm_resource_group.project-rg.name
+  address_space       = ["10.0.0.0/24"]
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Create three subnets with /27 CIDR blocks for each availability zone
+resource "azurerm_subnet" "az1_subnet" {
+  name                 = "az1-subnet"
+  resource_group_name  = azurerm_resource_group.project-rg.name
+  virtual_network_name = azurerm_virtual_network.project-vn.name
+  address_prefixes     = ["10.0.0.0/27"]
+}
+
+resource "azurerm_subnet" "az2_subnet" {
+  name                 = "az2-subnet"
+  resource_group_name  = azurerm_resource_group.project-rg.name
+  virtual_network_name = azurerm_virtual_network.project-vn.name
+  address_prefixes     = ["10.0.0.32/27"]
+}
+
+resource "azurerm_subnet" "az3_subnet" {
+  name                 = "az3-subnet"
+  resource_group_name  = azurerm_resource_group.project-rg.name
+  virtual_network_name = azurerm_virtual_network.project-vn.name
+  address_prefixes     = ["10.0.0.64/27"]
 }
