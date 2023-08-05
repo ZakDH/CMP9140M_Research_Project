67fed144899fc74b4e076417950ba72808cfdf57
ZakDH <zhargreaves1@gmail.com>
2023-08-05 22:55:30 +0100

    changes

--- Code/instances.tf
+++ Code/instances.tf
@@ -4,38 +4,28 @@ locals {
   }
 }
 
-resource "azurerm_linux_virtual_machine_scale_set" "web-vmss" {
+resource "azurerm_windows_virtual_machine_scale_set" "web-vmss" {
   name                = "web-vmss"
   resource_group_name = azurerm_resource_group.RG-UK-South.name
   location            = azurerm_resource_group.RG-UK-South.location
   sku                 = "Standard_F2"
   instances           = 3
   admin_username      = "adminuser"
-  user_data           = base64encode(templatefile("linux_userdata.tftpl", local.data_inputs))
+  admin_password      = "Azure@123"
+  upgrade_mode        = "Automatic"
   zones               = ["1", "2", "3"]
   zone_balance        = true
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
     version   = "latest"
-  }
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("vm.pub")
   }
 
   os_disk {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
   }
-
-  # custom_data = base64encode(<<EOF
-  # #!/bin/bash
-  # echo "Hello from the outside world!"
-  # EOF
-  # )
 
   network_interface {
     name    = "web-vmss-nic"
@@ -50,11 +40,55 @@ resource "azurerm_linux_virtual_machine_scale_set" "web-vmss" {
   }
 }
 
+resource "azurerm_storage_account" "appstore" {
+  name                     = "appstore457768709"
+  resource_group_name      = azurerm_resource_group.RG-UK-South.name
+  location                 = azurerm_resource_group.RG-UK-South.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  #allow_blob_public_access = true
+}
+
+resource "azurerm_storage_container" "data" {
+  name                  = "data"
+  storage_account_name  = "appstore457768709"
+  container_access_type = "blob"
+  depends_on = [
+    azurerm_storage_account.appstore
+  ]
+}
+
+resource "azurerm_storage_blob" "IIS_config" {
+  name                   = "IIS_Config.ps1"
+  storage_account_name   = "appstore457768709"
+  storage_container_name = "data"
+  type                   = "Block"
+  source                 = "IIS_Config.ps1"
+  depends_on             = [azurerm_storage_container.data]
+}
+
+resource "azurerm_virtual_machine_scale_set_extension" "scaleset_extension" {
+  name                         = "scaleset-extension"
+  virtual_machine_scale_set_id = azurerm_windows_virtual_machine_scale_set.web-vmss.id
+  publisher                    = "Microsoft.Compute"
+  type                         = "CustomScriptExtension"
+  type_handler_version         = "1.9"
+  depends_on = [
+    azurerm_storage_blob.IIS_config
+  ]
+  settings = <<SETTINGS
+    {
+        "fileUris": ["https://${azurerm_storage_account.appstore.name}.blob.core.windows.net/data/IIS_Config.ps1"],
+          "commandToExecute": "powershell -ExecutionPolicy Unrestricted -file IIS_Config.ps1"     
+    }
+SETTINGS
+}
+
 resource "azurerm_monitor_autoscale_setting" "example" {
   name                = "myAutoscaleSetting"
   resource_group_name = azurerm_resource_group.RG-UK-South.name
   location            = azurerm_resource_group.RG-UK-South.location
-  target_resource_id  = azurerm_linux_virtual_machine_scale_set.web-vmss.id
+  target_resource_id  = azurerm_windows_virtual_machine_scale_set.web-vmss.id
 
   notification {
     email {
@@ -84,7 +118,7 @@ resource "azurerm_monitor_autoscale_setting" "example" {
 
       metric_trigger {
         metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_linux_virtual_machine_scale_set.web-vmss.id
+        metric_resource_id = azurerm_windows_virtual_machine_scale_set.web-vmss.id
         time_grain         = "PT1M"
         statistic          = "Average"
         time_window        = "PT5M"
@@ -104,7 +138,7 @@ resource "azurerm_monitor_autoscale_setting" "example" {
 
       metric_trigger {
         metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_linux_virtual_machine_scale_set.web-vmss.id
+        metric_resource_id = azurerm_windows_virtual_machine_scale_set.web-vmss.id
         metric_namespace   = "microsoft.compute/virtualmachinescalesets"
         time_grain         = "PT1M"
         statistic          = "Average"
@@ -127,7 +161,7 @@ resource "azurerm_monitor_autoscale_setting" "example" {
 
       metric_trigger {
         metric_name        = "Available Memory Bytes"
-        metric_resource_id = azurerm_linux_virtual_machine_scale_set.web-vmss.id
+        metric_resource_id = azurerm_windows_virtual_machine_scale_set.web-vmss.id
         metric_namespace   = "microsoft.compute/virtualmachinescalesets"
         time_grain         = "PT1M"
         statistic          = "Average"
@@ -148,7 +182,7 @@ resource "azurerm_monitor_autoscale_setting" "example" {
 
       metric_trigger {
         metric_name        = "Available Memory Bytes"
-        metric_resource_id = azurerm_linux_virtual_machine_scale_set.web-vmss.id
+        metric_resource_id = azurerm_windows_virtual_machine_scale_set.web-vmss.id
         metric_namespace   = "microsoft.compute/virtualmachinescalesets"
         time_grain         = "PT1M"
         statistic          = "Average"
@@ -186,9 +220,9 @@ resource "azurerm_windows_virtual_machine" "business-vm" {
     version   = "latest"
   }
 
-  boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.primarystorage1.primary_blob_endpoint
-  }
+  # boot_diagnostics {
+  #   storage_account_uri = data.azurerm_storage_blob.primarystorage01
+  # }
 }
 # # Install IIS web server to the virtual machine
 # resource "azurerm_virtual_machine_extension" "web-server" {
--- Code/storage.tf
+++ Code/storage.tf
@@ -1,8 +1,14 @@
 # Create Azure Storage Account required for Function App
-resource "azurerm_storage_account" "primarystorage1" {
-  name                     = "primarystorage1"
-  resource_group_name      = azurerm_resource_group.RG-UK-South.name
-  location                 = azurerm_resource_group.RG-UK-South.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+# resource "azurerm_storage_account" "primarystorage1" {
+#   name                     = "primarystorage1"
+#   resource_group_name      = azurerm_resource_group.RG-UK-South.name
+#   location                 = azurerm_resource_group.RG-UK-South.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
+
+# data "azurerm_storage_blob" "primarystorage01" {
+#   name                   = "primarystorage01"
+#   storage_account_name   = "primarystorage01"
+#   storage_container_name = "primarystoragecontainer01"
+# }
