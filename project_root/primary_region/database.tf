1e100f3af1d0bf00d9263a04d4199b14e06da275
ZakDH <zhargreaves1@gmail.com>
2023-08-12 20:20:49 +0100

    .

--- project_root/primary_region/database.tf
+++ project_root/primary_region/database.tf
@@ -1,29 +1,29 @@
-resource "azurerm_mssql_server" "primary-web-tier-server01" {
-  name                = "primary-web-tier-server01"
-  location            = data.azurerm_resource_group.primary_rg.location
-  resource_group_name = data.azurerm_resource_group.primary_rg.name
+# resource "azurerm_mssql_server" "primary-web-tier-server01" {
+#   name                = "primary-web-tier-server01"
+#   location            = data.azurerm_resource_group.primary_rg.location
+#   resource_group_name = data.azurerm_resource_group.primary_rg.name
 
-  administrator_login          = "mysqladminun"
-  administrator_login_password = "H@Sh1CoR3!"
-  version                      = "12.0"
-}
+#   administrator_login          = "mysqladminun"
+#   administrator_login_password = "H@Sh1CoR3!"
+#   version                      = "12.0"
+# }
 
-resource "azurerm_mssql_database" "primary-web-tier-db01" {
-  name      = "primary-web-tier-db01"
-  server_id = azurerm_mssql_server.primary-web-tier-server01.id
-}
+# resource "azurerm_mssql_database" "primary-web-tier-db01" {
+#   name      = "primary-web-tier-db01"
+#   server_id = azurerm_mssql_server.primary-web-tier-server01.id
+# }
 
-resource "azurerm_mssql_server" "primary-business-tier-server01" {
-  name                = "primary-business-tier-server01"
-  location            = data.azurerm_resource_group.primary_rg.location
-  resource_group_name = data.azurerm_resource_group.primary_rg.name
+# resource "azurerm_mssql_server" "primary-business-tier-server01" {
+#   name                = "primary-business-tier-server01"
+#   location            = data.azurerm_resource_group.primary_rg.location
+#   resource_group_name = data.azurerm_resource_group.primary_rg.name
 
-  administrator_login          = "mysqladminun"
-  administrator_login_password = "H@Sh1CoR3!"
-  version                      = "12.0"
-}
+#   administrator_login          = "mysqladminun"
+#   administrator_login_password = "H@Sh1CoR3!"
+#   version                      = "12.0"
+# }
 
-resource "azurerm_mssql_database" "primary-business-tier-db01" {
-  name      = "primary-business-tier-db01"
-  server_id = azurerm_mssql_server.primary-business-tier-server01.id
-}
+# resource "azurerm_mssql_database" "primary-business-tier-db01" {
+#   name      = "primary-business-tier-db01"
+#   server_id = azurerm_mssql_server.primary-business-tier-server01.id
+# }
--- project_root/secondary_region/storage.tf
+++ project_root/secondary_region/storage.tf
@@ -1,73 +1,73 @@
-# STORAGE FOR SECONDARY WEB-VMSS
-resource "azurerm_storage_account" "secondary_web_storage" {
-  name                     = "secondaryvmssstorage"
-  resource_group_name      = data.azurerm_resource_group.secondary_rg.name
-  location                 = data.azurerm_resource_group.secondary_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+# # STORAGE FOR SECONDARY WEB-VMSS
+# resource "azurerm_storage_account" "secondary_web_storage" {
+#   name                     = "secondaryvmssstorage"
+#   resource_group_name      = data.azurerm_resource_group.secondary_rg.name
+#   location                 = data.azurerm_resource_group.secondary_rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
 
-  tags = {
-    environment = "dev"
-  }
-}
+#   tags = {
+#     environment = "dev"
+#   }
+# }
 
-resource "azurerm_storage_container" "secondary_web_container" {
-  name                  = "secondaryvmsscontainer"
-  storage_account_name  = "secondaryvmssstorage"
-  container_access_type = "blob"
-  depends_on            = [azurerm_storage_account.secondary_web_storage]
-}
+# resource "azurerm_storage_container" "secondary_web_container" {
+#   name                  = "secondaryvmsscontainer"
+#   storage_account_name  = "secondaryvmssstorage"
+#   container_access_type = "blob"
+#   depends_on            = [azurerm_storage_account.secondary_web_storage]
+# }
 
-resource "azurerm_storage_blob" "secondary_web_blob" {
-  name                   = "IIS_Config.ps1"
-  storage_account_name   = "secondaryvmssstorage"
-  storage_container_name = "secondaryvmsscontainer"
-  type                   = "Block"
-  source                 = "IIS_Config.ps1"
-  depends_on             = [azurerm_storage_container.secondary_web_container]
-}
+# resource "azurerm_storage_blob" "secondary_web_blob" {
+#   name                   = "IIS_Config.ps1"
+#   storage_account_name   = "secondaryvmssstorage"
+#   storage_container_name = "secondaryvmsscontainer"
+#   type                   = "Block"
+#   source                 = "IIS_Config.ps1"
+#   depends_on             = [azurerm_storage_container.secondary_web_container]
+# }
 
-# STORAGE FOR SECONDARY BUSINESS VMS
-resource "azurerm_storage_account" "secondary_business_storage" {
-  name                     = "secondarybusinessstorage"
-  resource_group_name      = data.azurerm_resource_group.secondary_rg.name
-  location                 = data.azurerm_resource_group.secondary_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+# # STORAGE FOR SECONDARY BUSINESS VMS
+# resource "azurerm_storage_account" "secondary_business_storage" {
+#   name                     = "secondarybusinessstorage"
+#   resource_group_name      = data.azurerm_resource_group.secondary_rg.name
+#   location                 = data.azurerm_resource_group.secondary_rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
 
-  tags = {
-    environment = "dev"
-  }
-}
+#   tags = {
+#     environment = "dev"
+#   }
+# }
 
-resource "azurerm_storage_container" "secondary_business_container" {
-  name                  = "secondarybusinesscontainer"
-  storage_account_name  = "secondarybusinessstorage"
-  container_access_type = "blob"
-  depends_on            = [azurerm_storage_account.secondary_business_storage]
-}
+# resource "azurerm_storage_container" "secondary_business_container" {
+#   name                  = "secondarybusinesscontainer"
+#   storage_account_name  = "secondarybusinessstorage"
+#   container_access_type = "blob"
+#   depends_on            = [azurerm_storage_account.secondary_business_storage]
+# }
 
-resource "azurerm_storage_blob" "secondary_business_blob" {
-  name                   = "IIS_Config.ps1"
-  storage_account_name   = "secondarybusinessstorage"
-  storage_container_name = "secondarybusinesscontainer"
-  type                   = "Block"
-  source                 = "IIS_Config.ps1"
-  depends_on             = [azurerm_storage_container.secondary_business_container]
-}
+# resource "azurerm_storage_blob" "secondary_business_blob" {
+#   name                   = "IIS_Config.ps1"
+#   storage_account_name   = "secondarybusinessstorage"
+#   storage_container_name = "secondarybusinesscontainer"
+#   type                   = "Block"
+#   source                 = "IIS_Config.ps1"
+#   depends_on             = [azurerm_storage_container.secondary_business_container]
+# }
 
-output "secondary_web_storage" {
-  value = azurerm_storage_account.secondary_web_storage.id
-}
+# output "secondarywebstorage" {
+#   value = azurerm_storage_account.secondary_web_storage.id
+# }
 
-output "secondary_web_container" {
-  value = azurerm_storage_container.secondary_web_container.name
-}
+# output "secondarywebcontainer" {
+#   value = azurerm_storage_container.secondary_web_container.name
+# }
 
-output "secondary_business_storage" {
-  value = azurerm_storage_account.secondary_business_storage.id
-}
+# output "secondarybusinessstorage" {
+#   value = azurerm_storage_account.secondary_business_storage.id
+# }
 
-output "secondary_business_container" {
-  value = azurerm_storage_container.secondary_business_container.name
-}
+# output "secondarybusinesscontainer" {
+#   value = azurerm_storage_container.secondary_business_container.name
+# }
